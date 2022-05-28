fd63891123f2bc5e85f1232c323d9efa83712fc2
leonardo62692 <leonardoalvarezp@outlook.com>
2022-05-28 14:09:47 -0500

    mainjs

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log( "hola perro");
+alert("hola")
+
+function permiso(nombre, edad){
+    console.log(nombre, edad)
+
+    if (edad >= 18) {
+        console.log(nombre + " puede solicitar permiso")
+        
+    }else(
+     console.log(nombre + " no puede solitar permiso")   
+    )
+}
+
+permiso("pedro",10)
+
